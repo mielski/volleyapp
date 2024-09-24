14b6098226175b4cfa75f889ff035b9fd2b4b576
happi <happiemiel@gmail.com>
2024-09-24 22:42:00 +0200

    successfully render a form from the example github documentation

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, flash, url_for, redirect, render_template
 from flask_bootstrap import Bootstrap5
+import flask_wtf
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -9,6 +12,21 @@ bootstrap = Bootstrap5(app)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/form', methods=["GET", "POST"])
+def test_form():
+    form = HelloForm()
+    if form.validate_on_submit():
+        flash("Form validated!")
+        return redirect(url_for("hello_world"))
+    return render_template("form.html",
+                           form=form)
+
+class HelloForm(flask_wtf.FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField()
 
 
 if __name__ == '__main__':
