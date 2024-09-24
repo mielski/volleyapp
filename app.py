70f9f07cce3b7de665beec7f5d60fe0e58e98310
happi <happiemiel@gmail.com>
2024-09-24 22:27:49 +0200

    succesfully added bootstrap extention added pipenv as dev dependency

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 
+bootstrap = Bootstrap5(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
