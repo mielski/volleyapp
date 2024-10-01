958d16483b3aec06810aca884e2a444a1974a188
happi <happiemiel@gmail.com>
2024-10-01 11:09:36 +0200

    added custom.css changed footer added description field in manual records in database_operations.py (wip) styling for the exercises in training_details_view.html

--- database_operations.py
+++ database_operations.py
@@ -23,11 +23,14 @@ def fetch_all():
     for entry in trainings.find({}):
         print(entry)
 
+
 def add_some_trainings():
     """Adds two trainings to the trainings collection"""
 
-    tm1 = TrainingModel(title="Passing - houding voeten", date="2024-09-20", rating=1)
-    tm2 = TrainingModel(title="Passing - beweging naar bal", date="2024-09-27", rating=1)
+    tm1 = TrainingModel(title="Passing - houding voeten", date="2024-09-20", rating=1,
+                        description="Doel: oefenen op het klaarstaan voor de bal met juiste houding voetenwerk.")
+    tm2 = TrainingModel(title="Passing - beweging naar bal", date="2024-09-27", rating=1,
+                        description="Doel: oefenen op balbaanbehoordeling en achter de bal komen")
 
     trainings.insert_one(tm1.model_dump(by_alias=True))
     trainings.insert_one(tm2.model_dump(by_alias=True))
