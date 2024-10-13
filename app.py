a6d47d52c7deecca68b6a0cd71651b0b0042e223
happi <happiemiel@gmail.com>
2024-10-13 21:28:30 +0200

    it's starting to look like something start working on editing the training detais via a form

--- app.py
+++ app.py
@@ -40,12 +40,35 @@ def view_trainings():
     return render_template("trainings.html",
                            trainings=trainings, title="Trainings")
 
-@app.route("/training/<string:id_>", methods=["GET","POST"])
+@app.route("/training/<string:id_>/view", methods=["GET"])
 def training_details_view(id_):
     """endpoint to view the training details in a well formatted html."""
 
     # load the item
     training_item = app.db.trainings.find_one({"_id": id_})
+    if training_item is None:
+        abort(404, "the requested id does not exist")
+    training = TrainingModel(**training_item)
+
+    # render the item
+    return render_template("training_details_view.html",
+                           training=training, title="View Training")
+@app.route("/training/<string:id_>/edit", methods=["GET","POST"])
+def training_details_edit(id_):
+    """endpoint to edit the training details in a well formatted html."""
+
+    training_item = app.db.trainings.find_one({"_id": id_})
+
+    form = TrainingFormDetailed(**training_item)
+    if form.validate_on_submit():
+        training = TrainingModel(**form.data)
+
+        #add training to database
+        training_dict = training.model_dump(by_alias=True)
+        app.db.trainings.update_one({"id_": id_}, training_dict)
+        return redirect(url_for("training_details_view", id_=id_))
+
+    # load the item
     if training_item is None:
         abort(404, "the requested id does not exist")
     training = TrainingModel(**training_item)
