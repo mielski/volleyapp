49302138ce8bafd83a93b88061d176b8dbd9e31b
happi <happiemiel@gmail.com>
2025-02-18 21:42:37 +0100

    use class examples for easier testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,21 +7,26 @@ from models import DifficultyLevel
 
 class TestExerciseLink:
 
+    @pytest.fixture(autouse=True)
+    def examples(self):
+
+        self.by_ref = models.ExerciseLink(ref_id="3")
+        self.by_model = models.ExerciseLink(model={"title": "hello there", "difficulty_level": "Beginner"})
+
     def test_model_vanilla(self):
 
         ex_link = models.ExerciseLink(ref_id="3")
 
-        assert ex_link.ref_id == "3"
-        assert ex_link.model is None
+        assert self.by_ref.ref_id == "3"
+        assert self.by_ref.model is None
 
 
     def test_model_data_proper(self):
 
-        ex = models.ExerciseLink(model={"title": "hello there", "difficulty_level": "Beginner"})
-
-        assert ex.model.title == "hello there"
-        assert ex.model.approach == ""
-        assert ex.model.difficulty_level == DifficultyLevel.beginner
+        assert self.by_model.model.title == "hello there"
+        assert self.by_model.model.approach == ""
+        assert self.by_model.model.difficulty_level == DifficultyLevel.beginner
+        assert self.by_model.ref_id is None
 
     def test_model_requires_1_argument(self):
         """tests post validation that requires either the ref_id or the model to be defined."""
